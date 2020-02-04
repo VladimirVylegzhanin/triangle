bedc5c4327c17da1b4fb872128b92ea97b61a514
Vladimir Vylegzhanin <v.vylegzhanin@dodopizza.com>
2020-02-04 16:07:12 +0300

    make TriangleTypeAnalyzer static, because it havent any state

--- TriangleTask/TriangleTask.Tests/TriangleAnalyzerShould.cs
+++ TriangleTask/TriangleTask.Tests/TriangleAnalyzerShould.cs
@@ -5,29 +5,21 @@ namespace TriangleTask.Tests
 	[TestFixture]
 	public class TriangleAnalyzerShould
 	{
-		private TriangleTypeAnalyzer _analyzer;
-
-		[SetUp]
-		public void SetUp()
-		{
-			_analyzer = new TriangleTypeAnalyzer();
-		}
-		
 		[TestCase(10, 1 ,1)]
 		[TestCase(1, 10,1)]
 		[TestCase(1, 1,10)]
-		public void ReturnUndefinedIfOneSideMoreThatSumOfTwo(double a, double b, double c)
+		public void ReturnUndefinedIfOneSideMoreThatSumOfTwo(decimal a, decimal b, decimal c)
 		{
-			var result = _analyzer.AnalyzeTriangle(a, b, c);
+			var result = TriangleTypeAnalyzer.AnalyzeTriangle(a, b, c);
 			Assert.That(result, Is.EqualTo(TriangleType.Undefined));
 		}
 		
 		[TestCase(3, 2,2)]
 		[TestCase(2, 3,2)]
 		[TestCase(2, 2,3)]
-		public void ReturnObtuseIfOneSideSquaredThatSquaredSumOfTwo(double a, double b, double c)
+		public void ReturnObtuseIfOneSideSquaredThatSquaredSumOfTwo(decimal a, decimal b, decimal c)
 		{
-			var result = _analyzer.AnalyzeTriangle(a, b, c);
+			var result = TriangleTypeAnalyzer.AnalyzeTriangle(a, b, c);
 			Assert.That(result, Is.EqualTo(TriangleType.Obtuse));
 		}
 	}
--- TriangleTask/TriangleTask/TriangleTypeAnalyzer.cs
+++ TriangleTask/TriangleTask/TriangleTypeAnalyzer.cs
@@ -1,34 +1,25 @@
-﻿using System;
-
-namespace TriangleTask
+﻿namespace TriangleTask
 {
-	public class TriangleTypeAnalyzer
+	public static class TriangleTypeAnalyzer
 	{
-		private const double Epsilon = 0.0001;
-
-		public TriangleType AnalyzeTriangle(double a, double b, double c)
+		public static TriangleType AnalyzeTriangle(decimal a, decimal b, decimal c)
 		{
-			checked
+			if (a + b <= c || a + c <= b || b + c <= a)
 			{
-				if (a + b <= c || a + c <= b || b + c <= a)
-				{
-					return TriangleType.Undefined;
-				}
-
-				if (Math.Abs(a * a - (b * b + c * c)) < Epsilon 
-					|| Math.Abs(b * b - (c * c + a * a)) < Epsilon 
-					|| Math.Abs(c * c - (a * a + b * b)) < Epsilon)
-				{
-					return TriangleType.Right;
-				}
-
-				if (a * a > b * b + c * c || c * c > a * a + b * b || b * b > a * a + c * c)
-				{
-					return TriangleType.Obtuse;
-				}
-
-				return TriangleType.Acute;
+				return TriangleType.Undefined;
 			}
+
+			if (a * a == b * b + c * c || b * b == c * c + a * a || c * c == a * a + b * b)
+			{
+				return TriangleType.Right;
+			}
+
+			if (a * a > b * b + c * c || c * c > a * a + b * b || b * b > a * a + c * c)
+			{
+				return TriangleType.Obtuse;
+			}
+
+			return TriangleType.Acute;
 		}
 	}
 }
